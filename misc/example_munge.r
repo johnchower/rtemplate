49bb50dd4dd1695ffc0aa4df7f4c6add06155ec8
johnchower <johnchower@gmail.com>
2017-07-10 13:51:17 -0600

    Improve example_munge.r preamble.

--- misc/example_munge.r
+++ misc/example_munge.r
@@ -3,38 +3,48 @@
 # The makefile takes care of this stuff when running in batch mode, but it's 
 # useful when developing the script.
 if (interactive()){
-  # Set the following 3 variables before running:
-  projname <- "rtemplate"
-  # Cached datasets to use
-  to_link_cacheReserve <- list()
-  # Raw datasets to use
-  to_link_dataReserve <- list("test_data.csv")
-
-  proj_root <- rprojroot::find_root(
-    rprojroot::has_dirname(projname)
-  )
-  setwd(proj_root)
-
-  # Load project packages and helper functions
-  ProjectTemplate::load.project()
-
-  # Link datasets into appropriate primary folder
-  lA.link_datasets(
-    raw_list = to_link_dataReserve
-  , cached_list = to_link_cacheReserve
-  , proj_root = proj_root
-  )
-
-  ProjectTemplate::load.project()
-
-  rm(to_link_dataReserve, to_link_cacheReserve)
-}
-
-# Put your code here. It should return a single object, called munge_out
-
-# End of your code 
-
-if (interactive()){
+# Set the following 3 variables before running: a
+projname <- "rtemplate"
+# Cached datasets to use
+# Enter full name of dataset, without path
+# For example, m0.advanced_invites_sent.RData
+to_link_cacheReserve <- list(
+)
+# Raw datasets to use
+# Enter full name of dataset, without path
+# For example, advanced_invites_sent.csv
+to_link_dataReserve <- list(
+)
+# Find the project's root directory
+proj_root <- rprojroot::find_root(
+  rprojroot::has_dirname(projname)
+)
+# Clean out existing symlinks in directory
+system(paste0(
+  "cd "
+, gsub(proj_root, pattern = " ", replacement = "\\\\ ")
+, " ; "
+, "make clean"
+))
+# Load project packages and functions
 setwd(proj_root)
-system("make clean")
+suppressMessages(ProjectTemplate::load.project())
+# Link datasets into appropriate primary folder
+lA.link_datasets(
+  raw_list = to_link_dataReserve
+, cached_list = to_link_cacheReserve
+, proj_root = proj_root
+)
+# Load project data
+suppressMessages(ProjectTemplate::load.project())
+# Clean up
+rm(to_link_dataReserve, to_link_cacheReserve)
+system(paste0(
+  "cd "
+, gsub(proj_root, pattern = " ", replacement = "\\\\ ")
+, " ; "
+, "make clean"
+))
 }
+
+# Put your code below. It should return a single object, called munge_out
