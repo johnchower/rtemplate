081348c85dd663b3db9b06dd468a4a1f9349af02
johnchower <johnchower@gmail.com>
2017-07-20 13:51:09 -0600

    Fix install script

--- src/install_r_packages.r
+++ src/install_r_packages.r
@@ -3,26 +3,31 @@ if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
 
-projname <- "rtemplate"
+projname <- "active_user_rankings"
 proj_root <- rprojroot::find_root(
   rprojroot::has_dirname(projname)
 )
 setwd(proj_root)
 package_list <- readLines("misc/package_list.r")
 
+strsplit(package_list[19], "/")[[1]][2]
 lapply(
   package_list
 , FUN = function(name) {
-    already_installed <- !require(name,character.only = TRUE)
-    if (already_installed){
-      github_package <- grepl(
-        pattern = " github$"
-      , x = name
-      )
+    github_package <- grepl(
+      pattern = " github$"
+    , x = name
+    )
+    if (github_package) {
+      package_info <- gsub(" github", "", name)
+      package_name <- strsplit(package_info, "/")[[1]][2]
+    } else {
+      package_name <- name
+    }
+    not_already_installed <- !require(package_name,character.only = TRUE)
+    if (not_already_installed){
       if (github_package) {
-        package_info <- gsub(" github", "", name)
-        print(package_info)
-        devtools::install_github(repo = )
+        devtools::install_github(repo = package_info)
       }
       else {install.packages(name)}
     }
