1fd51003a7c00f56d2820f1d898dd95e74c80c64
johnchower <johnchower@gmail.com>
2017-07-20 10:23:35 -0600

    Add ProjectTemplate to the list of required packages.

--- src/install_r_packages.r
+++ src/install_r_packages.r
@@ -1,4 +1,4 @@
-packages <- c("devtools", "knitr", "ezknitr", "rprojroot")
+packages <- c("devtools", "knitr", "ezknitr", "rprojroot", "ProjectTemplate")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
